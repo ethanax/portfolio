ada119224a93e51ffd3edb810a7c48a934d090f5
ethanax <ethanxin@g.ucla.edu>
2026-08-01 20:47:51 -0500

    Create initial portfolio components and starter home page

---
+++ components/navbar.jsx
@@ -0,0 +1,12 @@
+export default function Navbar(){
+    return(
+        <nav>
+            <h1>Ethan Xin</h1>
+            <div>
+                <a href="#about">About</a>
+                <a href="#projects">Projects</a>
+                <a href="#contact">Contact</a>
+            </div>
+        </nav>
+    );
+}
