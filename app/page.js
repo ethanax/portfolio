ee74e34511b0ef24be376a1761b3560a6f3f4662
ethanax <ethan.a.xin@gmail.com>
2026-08-02 11:31:54 -0500

    Align name header to top left with navbar pages to top right

--- app/page.js
+++ app/page.js
@@ -1,18 +1,20 @@
 import Image from "next/image";
-import Navbar from "@/components/navbar";
+import Navbar from "@/components/Navbar";
 
 export default function Home() {
   return (
-    <main>
-      <navbar />
+    <>
+      <Navbar />
 
-      <h1 className="txt-6xl font-bold">
-        Hi, I'm Ethan.
-      </h1>
+      <main>
+        <h1 className="txt-6xl font-bold">
+          Hi, I'm Ethan!
+        </h1>
 
-      <p className="mt-4 text-lg text-gray-600">
-        First year Computer Science Student at UCLA building software for social impact.
-      </p>
-    </main>
+        <p className="mt-4 text-lg text-gray-600">
+          First year Computer Science Student at UCLA building software for social impact. 676767676767
+        </p>
+      </main>
+    </>
   );
 }
--- components/navbar.jsx
+++ components/navbar.jsx
@@ -1,11 +1,12 @@
 export default function Navbar(){
     return(
-        <nav>
-            <h1>Ethan Xin</h1>
-            <div>
-                <a href="#about">About</a>
-                <a href="#projects">Projects</a>
-                <a href="#contact">Contact</a>
+        <nav className="flex w-full items-start justify-between px-4 pt-4">
+            <h1 className="text-xl font-semibold">Ethan Xin</h1>
+            <div className="flex items-center gap-6">
+                <a href="home">Home</a>
+                <a href="about">About</a>
+                <a href="projects">Projects</a>
+                <a href="contact">Contact</a>
             </div>
         </nav>
     );
